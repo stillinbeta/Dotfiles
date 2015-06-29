a90772bead18f677c168b5586a8d5979ffece9be
liz <liz@heroku.com>
2015-06-29 16:30:13 -0400

    Keyboard script runs as root

--- Scripts/keyboard.sh
+++ Scripts/keyboard.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+echo $(id -un)
+[ $(id -un) != 'liz' ] && exec su -c $0 liz
+
+export DISPLAY=:0
+
 LAPTOP=`xinput list --id-only "keyboard:AT Translated Set 2 keyboard"`
 
 if EXTERNAL=$(xinput list --id-only "keyboard:Lenovo ThinkPad Compact USB Keyboard with TrackPoint"); then
     echo "External found, setting value"
-    setxkbmap -layout dvorak -option caps:escape -device $EXTERNAL
+    setxkbmap -layout dvorak -option caps:escape -device $EXTERNAL -display $DISPLAY
 fi
 
 echo 'test'
-setxkbmap -layout dvorak -device $LAPTOP
-xkbcomp ~/.xkb -i $LAPTOP $DISPLAY
-
-
-
+setxkbmap -layout dvorak -device $LAPTOP -display $DISPLAY
+xkbcomp /home/liz/.xkb -i $LAPTOP $DISPLAY
