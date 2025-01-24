cb7a44b25460e9abf5e6246f9cce3a87cef0f72e
Ellie Frost <web@stillinbeta.com>
2025-01-23 17:59:31 -0800

    stylix 24.11

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix/release-24.11";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, stylix, ... }: {
