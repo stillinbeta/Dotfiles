9e3610ae2f1e606d25fe8b89e051cd94d6486400
Ellie Frost <web@stillinbeta.com>
2025-01-23 18:00:34 -0800

    dark mode and monospace

--- stylix.nix
+++ stylix.nix
@@ -3,5 +3,10 @@
   stylix = {
     enable = true;
     image = Pictures/Commission_Beta.png;
+    polarity = "dark";
+    fonts.monospace = {
+      name = "Inconsolata";
+      package = pkgs.inconsolata;
+    };
   };
 }
