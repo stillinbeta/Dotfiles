dfbd4e312de5a0315ae3cd086d6c46929972dad6
Ellie Frost <web@stillinbeta.com>
2025-02-03 21:49:53 -0800

    cachix

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,7 @@
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./cachix.nix
     # ./sway-gnome.nix
     # /home/ellie/suspend-and-hibernate.nix
   ];
