bc9e89f1a749ad1444ca09e85fdb7f4d8bea89e0
Ellie Frost <web@stillinbeta.com>
2025-04-02 19:30:57 -0700

    transparent

--- stylix.nix
+++ stylix.nix
@@ -3,10 +3,17 @@
   stylix = {
     enable = true;
     image = Pictures/Commission_Beta.png;
+    opacity = {
+      terminal = 0.85;
+      applications = 0.85;
+    };
     polarity = "dark";
     fonts.monospace = {
       name = "Inconsolata";
       package = pkgs.inconsolata;
+    };
+    fonts.sizes = {
+      terminal = 16;
     };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/outrun-dark.yaml";
   };
