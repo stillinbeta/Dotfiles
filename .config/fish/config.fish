215257a0b30fbcdddf9f6962544b0956a4c4e2ab
Liz Frost <liz@upbound.io>
2026-02-06 16:01:56 -0800

    install jolt and asdf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,3 +28,21 @@ status is-interactive; and begin
     direnv hook fish | source
 
 end
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+if not contains $HOME/.local/bin $PATH
+    set -gx --prepend PATH $HOME/.local/bin
+end
