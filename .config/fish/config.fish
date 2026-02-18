bcb044c0a47d24eb715d530f578dc6a42833bfbe
Liz Frost <liz@upbound.io>
2026-02-18 12:07:23 -0800

    path config before completion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,17 @@ status is-login; and begin
 
 end
 
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+if not contains $HOME/.local/bin $PATH
+    set -gx --prepend PATH $HOME/.local/bin
+end
+
 status is-interactive; and begin
 
     # Abbreviations
@@ -29,23 +40,4 @@ status is-interactive; and begin
     helmfile completion fish | source
 
     abbr --add k kubectl
-end
-
-
-# ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-
-if not contains $HOME/.local/bin $PATH
-    set -gx --prepend PATH $HOME/.local/bin
 end
