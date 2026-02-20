fb2cbec83fc302517e6367ec1299d7115d4c6011
Liz Frost <liz@upbound.io>
2026-02-19 16:23:21 -0800

    actually set up asdf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,12 @@ status is-login; and begin
 
 end
 
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
 # Do not use fish_add_path (added in Fish 3.2) because it
 # potentially changes the order of items in PATH
 if not contains $_asdf_shims $PATH
