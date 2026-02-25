8733c0d81c61d69ea2b73af3544c5f00d3db1bea
Liz Frost <liz@upbound.io>
2026-02-25 11:17:27 -0800

    aws completion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,6 +25,20 @@ if not contains $HOME/.local/bin $PATH
     set -gx --prepend PATH $HOME/.local/bin
 end
 
+function __aws_complete
+    set --local --export COMP_SHELL fish
+    set --local --export COMP_LINE (commandline -pc)
+
+    if string match -q -- - (commandline -pt)
+        set COMP_LINE "$COMP_LINE-"
+    end
+
+    aws_completer | command sed 's/ $//'
+end
+
+# Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+complete --command aws --no-files --arguments '(__aws_complete)'
+
 status is-interactive; and begin
 
     # Abbreviations
